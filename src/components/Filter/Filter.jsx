4169aeb8b238e38ca79d0b6a7bd21fd3cb622ea8
elen-purple <anna.havrylova.2011@gmail.com>
2026-04-05 10:02:27 +0300

    Update Filter.jsx

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,13 +1,16 @@
 import { useDispatch, useSelector } from 'react-redux';
 import { changeFilter } from 'redux/filter/filterReducer';
+import { selectFilter } from 'redux/filter/filterSelector';
 
 export const Filter = () => {
   const dispatch = useDispatch();
+  const filter = useSelector(selectFilter);
   return (
     <div>
       <input
         type="text"
         placeholder="Filter"
+        defaultValue={filter}
         onInput={e => {
           dispatch(changeFilter(e.currentTarget.value));
         }}
